0621af1cab611ce1fd538e5ca9266b3b6894852e
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-06-25 16:39:44 -0400

    Fixed Issues with MariaDB on Ubuntu 18.04
    Added /etc/mysql/mariadb.conf.d/50-server.cnf as another config file to check.

--- install.sh
+++ install.sh
@@ -536,18 +536,23 @@ install_pterodactyl() {
 
     output "Binding MariaDB/MySQL to 0.0.0.0."
 	if [ -f /etc/mysql/my.cnf ] ; then
-        sed -i -- 's/bind-address/# bind-address/g' /etc/mysql/my.cnf
+        	sed -i -- 's/bind-address/# bind-address/g' /etc/mysql/my.cnf
 		sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/mysql/my.cnf
 		output 'Restarting MySQL process...'
 		service mysql restart
 	elif [ -f /etc/my.cnf ] ; then
-        sed -i -- 's/bind-address/# bind-address/g' /etc/my.cnf
+        	sed -i -- 's/bind-address/# bind-address/g' /etc/my.cnf
 		sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/my.cnf
 		output 'Restarting MySQL process...'
 		service mysql restart
     	elif [ -f /etc/mysql/my.conf.d/mysqld.cnf ] ; then
-        sed -i -- 's/bind-address/# bind-address/g' /etc/my.cnf
-		sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/my.cnf
+        	sed -i -- 's/bind-address/# bind-address/g' /etc/mysql/my.conf.d/mysqld.cnf
+		sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/mysql/my.conf.d/mysqld.cnf
+		output 'Restarting MySQL process...'
+		service mysql restart
+    	elif [ -f /etc/mysql/mariadb.conf.d/50-server.cnf ] ; then
+        	sed -i -- 's/bind-address/# bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+		sed -i '/\[mysqld\]/a bind-address = 0.0.0.0' /etc/mysql/mariadb.conf.d/50-server.cnf
 		output 'Restarting MySQL process...'
 		service mysql restart
 	else 
