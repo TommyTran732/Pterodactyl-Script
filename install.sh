ca8b3d4fe7fec5f763186317b4c53bd7e8f1bff8
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-10-06 13:18:16 -0400

    Change Discord to Telegram
    Gotta reduce usage of big brother's software

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ warn(){
 preflight(){
     output "Pterodactyl Installation & Upgrade Script"
     output "Copyright © 2020 Thien Tran <contact@thientran.io>."
-    output "Please join my Discord for community support: https://thientran.io/discord"
+    output "Please join my Telegram for community support: https://t.me/revenact"
     output ""
 
     output "Please note that this script is meant to be installed on a fresh OS. Installing it on a non-fresh OS may cause problems."
