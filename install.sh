b2a9a8ffaf660e49b9a9e9b00c41621b7dece62f
Tommy <contact@tommytran.io>
2021-11-01 23:44:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -485,11 +485,13 @@ nginx_config() {
 echo '
 server {
     listen 80 default_server;
+    listen [::]:80 default_server;
     server_name '"$FQDN"';
     return 301 https://$server_name$request_uri;
 }
 server {
     listen 443 ssl http2 default_server;
+    listen [::]:443 ssl http2 default_server;
     server_name '"$FQDN"';
     root /var/www/pterodactyl/public;
     index index.php;
@@ -548,11 +550,13 @@ nginx_config_redhat(){
 echo '
 server {
     listen 80 default_server;
+    listen [::]:80 default_server;
     server_name '"$FQDN"';
     return 301 https://$server_name$request_uri;
 }
 server {
     listen 443 ssl http2 default_server;
+    listen [::]:443 ssl http2 default_server;
     server_name '"$FQDN"';
     root /var/www/pterodactyl/public;
     index index.php;
